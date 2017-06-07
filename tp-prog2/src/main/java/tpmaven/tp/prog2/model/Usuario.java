7f8d071b9b50919bc45911a1e94cf2e658ff3b1e
Mati Rey <soloamimepasa@hotmail.com>
2017-06-07 19:55:56 -0300

    modelo

---
+++ tp-prog2/src/main/java/tpmaven/tp/prog2/model/Usuario.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tpmaven.tp.prog2.model;
+
+/**
+ *
+ * @author Matias
+ */
+
+public class Usuario {
+
+    private int id;
+    private String nombreUsuario;
+    private String password;
+
+   public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
